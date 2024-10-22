6d9791bfbfe2b9a185a17a5ce7588334993ee47b
danishrauf400 <danishrauf400@gmail.com>
2024-10-22 16:14:20 +0500

    mid

---
+++ midterm/app.js
@@ -0,0 +1,7 @@
+function showIntro() {
+    document.getElementById('introduction').style.display = 'block';
+}
+
+function hideIntro() {
+    document.getElementById('introduction').style.display = 'none';
+}
